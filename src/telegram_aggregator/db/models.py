5d06bceefd1e60deb93c5b57334b415b1cc46f47
Konstantin Verner <52051213+konverner@users.noreply.github.com>
2024-09-28 13:29:34 +0200

    Update models.py

--- src/telegram_aggregator/db/models.py
+++ src/telegram_aggregator/db/models.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, String, DateTime
+from sqlalchemy import Column, DateTime, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
@@ -6,8 +6,14 @@ Base = declarative_base()
 class Message(Base):
     __tablename__ = 'messages'
 
-    time = Column(DateTime, primary_key=True)
-    message_text = Column(String)
-    message_id = Column(Integer)
+    id = Column(Integer, primary_key=True)
+    datetime = Column(DateTime)
+    content = Column(String)
     channel_name = Column(String)
 
+class Channel(Base):
+    __tablename__ = "channels"
+
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    name = Column(String)
+    comment = Column(String)
